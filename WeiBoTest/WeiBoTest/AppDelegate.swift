1e3fe211a6362bfefab0443e21e0f885c3a024f9
dingfude <dingfude@dingfudedemacbook-pro.local>
2016-12-21 22:43:17 +0800

    【ADD】初始化项目架构

--- WeiBoTest/WeiBoTest/AppDelegate.swift
+++ WeiBoTest/WeiBoTest/AppDelegate.swift
@@ -17,7 +17,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        sleep(4)
+        
+        window = UIWindow()
+        window?.backgroundColor = UIColor.white
+        window?.rootViewController = DFMainViewController()
+        window?.makeKeyAndVisible()
+        
+        
         
         return true
     }
