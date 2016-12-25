fde13a89f0c9f3a845a8dc7f9d39b3a7e74fa832
dingfude <dingfude@dingfudedeMacBook-Pro.local>
2016-12-25 11:34:53 +0800

    【Update】提交主分支

--- WeiBoTest/WeiBoTest/Classes/View(视图和控制器)/Home/DFHomeViewController.swift
+++ WeiBoTest/WeiBoTest/Classes/View(视图和控制器)/Home/DFHomeViewController.swift
@@ -35,7 +35,7 @@ class DFHomeViewController: DFBaseViewController {
     
     override func loadData() {
         
-        for i in 0..<15 {
+        for i in 0..<19 {
             
 //            statusList.insert(i.description, at: 0)
             
